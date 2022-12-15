c0e78e9634600de5edc941cfd2e0aa07618a94bf
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2022-12-15 13:01:14 +0700

    Add files via upload

---
+++ 1_B_71220866.py
@@ -0,0 +1,24 @@
+import turtle
+
+s = turtle.Screen()
+t = turtle.Turtle()
+
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.right(180)
+t.forward(100)
+t.penup()
+t.forward(50)
+t.pendo wn()
+t.circle(-25, 180)
+t.circle(30)
+t.circle(-25, 180)
+t.penup()
+t.forward(100)
+t.pendown()
+t.left(180)
+t.circle(-80, 80)
+
+s.exitonclick()
